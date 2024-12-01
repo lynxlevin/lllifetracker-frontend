6d47dc676468ad2a3323dce392ccfe125849edd1
lynxlevin <72173230+lynxlevin@users.noreply.github.com>
2024-12-01 21:21:18 +0900

    Redirect to Ambitions page if logged in at top page

---
+++ src/pages/Top.tsx
@@ -0,0 +1,22 @@
+import { Navigate, Link } from 'react-router-dom';
+import useUserAPI from '../hooks/useUserAPI';
+// import AppIcon from '../components/AppIcon';
+
+const Top = () => {
+    const { isLoggedIn } = useUserAPI();
+
+    if (isLoggedIn === true) {
+        return <Navigate to='/ambitions' />;
+    }
+    return (
+        <div style={{ fontSize: '24px' }}>
+            <br />
+            <Link to='/login'>Login</Link>
+            <br />
+            <br />
+            <Link to='/ambitions'>Ambitions</Link>
+        </div>
+    );
+};
+
+export default Top;
