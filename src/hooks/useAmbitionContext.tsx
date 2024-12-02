078a85a8c844a550550bf4e2c3623a78111de7af
lynxlevin <72173230+lynxlevin@users.noreply.github.com>
2024-12-02 11:59:01 +0900

    Add desctiption to create/update Action

--- src/hooks/useAmbitionContext.tsx
+++ src/hooks/useAmbitionContext.tsx
@@ -67,8 +67,8 @@ const useAmbitionContext = () => {
         });
     };
 
-    const addAction = (objectiveId: string, name: string) => {
-        ActionAPI.create({ name }).then(res => {
+    const addAction = (objectiveId: string, name: string, description: string | null) => {
+        ActionAPI.create({ name, description }).then(res => {
             const action = res.data;
             ObjectiveAPI.linkAction(objectiveId, action.id).then(_ => {
                 getAmbitionsWithLinks();
@@ -76,8 +76,8 @@ const useAmbitionContext = () => {
         });
     };
 
-    const updateAction = (id: string, name: string) => {
-        ActionAPI.update(id, { name }).then(_ => {
+    const updateAction = (id: string, name: string, description: string | null) => {
+        ActionAPI.update(id, { name, description }).then(_ => {
             getAmbitionsWithLinks();
         });
     };
