df8aa43182a3bd2ccce6e82e84b1da5ebec615ec
lynxlevin <72173230+lynxlevin@users.noreply.github.com>
2024-12-24 21:36:15 +0900

    Refactor TabBars

--- src/components/AmbitionsTabBar.tsx
+++ src/components/AmbitionsTabBar.tsx
@@ -8,34 +8,26 @@ function a11yProps(index: number) {
     };
 }
 
+const pathNames = [
+    { name: '/ambitions', label: '大望' },
+    { name: '/objectives', label: '目標' },
+    { name: '/actions', label: '行動' },
+];
+
 const AmbitionsTabBar = () => {
     const navigate = useNavigate();
     const location = useLocation();
-    const getValue = () => {
-        if (location.pathname === '/ambitions') return 0;
-        if (location.pathname === '/objectives') return 1;
-        if (location.pathname === '/actions') return 2;
-    };
-    const value = getValue();
     const handleChange = (_: React.SyntheticEvent, newValue: number) => {
-        if (newValue === 0) {
-            navigate('/ambitions');
-            window.scroll({ top: 0 });
-        } else if (newValue === 1) {
-            navigate('/objectives');
-            window.scroll({ top: 0 });
-        } else if (newValue === 2) {
-            navigate('/actions');
-            window.scroll({ top: 0 });
-        }
+        navigate(pathNames[newValue].name);
+        window.scroll({ top: 0 });
     };
 
     return (
         <Box sx={{ borderBottom: 1, borderColor: 'divider', backgroundColor: 'background.default' }}>
-            <Tabs value={value} onChange={handleChange} variant='fullWidth'>
-                <Tab label='大望' {...a11yProps(0)} />
-                <Tab label='目標' {...a11yProps(1)} />
-                <Tab label='行動' {...a11yProps(2)} />
+            <Tabs value={pathNames.findIndex(item => item.name === location.pathname)} onChange={handleChange} variant='fullWidth'>
+                {pathNames.map((item, i) => (
+                    <Tab key={`simple-tab-${item.name}`} label={item.label} {...a11yProps(i)} />
+                ))}
             </Tabs>
         </Box>
     );
--- src/components/MemosTabBar.tsx
+++ src/components/MemosTabBar.tsx
@@ -8,34 +8,26 @@ function a11yProps(index: number) {
     };
 }
 
+const pathNames = [
+    { name: '/memos', label: 'メモ' },
+    { name: '/mission-memos', label: '課題' },
+    { name: '/book-excerpts', label: '本の抜粋' },
+];
+
 const MemosTabBar = () => {
     const navigate = useNavigate();
     const location = useLocation();
-    const getValue = () => {
-        if (location.pathname === '/memos') return 0;
-        if (location.pathname === '/mission-memos') return 1;
-        if (location.pathname === '/book-excerpts') return 2;
-    };
-    const value = getValue();
     const handleChange = (_: React.SyntheticEvent, newValue: number) => {
-        if (newValue === 0) {
-            navigate('/memos');
-            window.scroll({ top: 0 });
-        } else if (newValue === 1) {
-            navigate('/mission-memos');
-            window.scroll({ top: 0 });
-        } else if (newValue === 2) {
-            navigate('/book-excerpts');
-            window.scroll({ top: 0 });
-        }
+        navigate(pathNames[newValue].name);
+        window.scroll({ top: 0 });
     };
 
     return (
         <Box sx={{ borderBottom: 1, borderColor: 'divider', backgroundColor: 'background.default' }}>
-            <Tabs value={value} onChange={handleChange} variant='fullWidth'>
-                <Tab label='メモ' {...a11yProps(0)} />
-                <Tab label='課題' {...a11yProps(1)} />
-                <Tab label='本の抜粋' {...a11yProps(2)} />
+            <Tabs value={pathNames.findIndex(item => item.name === location.pathname)} onChange={handleChange} variant='fullWidth'>
+                {pathNames.map((item, i) => (
+                    <Tab key={`simple-tab-${item.name}`} label={item.label} {...a11yProps(i)} />
+                ))}
             </Tabs>
         </Box>
     );
