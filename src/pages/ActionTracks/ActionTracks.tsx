a1ecbe1f40fbadf94d979ee34eaec421fe102382
lynxlevin <72173230+lynxlevin@users.noreply.github.com>
2025-01-26 14:21:44 +0900

    UI for start button

--- src/pages/ActionTracks/ActionTracks.tsx
+++ src/pages/ActionTracks/ActionTracks.tsx
@@ -1,28 +1,50 @@
-import { Box, Grid2 as Grid, Stack, Typography } from '@mui/material';
+import { Box, Card, Grid2 as Grid, IconButton, Stack, Typography } from '@mui/material';
 import { useEffect } from 'react';
 import useUserAPI from '../../hooks/useUserAPI';
 import BasePage from '../../components/BasePage';
 import ActionTrack from './ActionTrack';
 import useActionTrackContext from '../../hooks/useActionTrackContext';
 import ActiveActionTrack from './ActiveActionTrack';
+import useActionContext from '../../hooks/useActionContext';
+import PlayArrowIcon from '@mui/icons-material/PlayArrow';
 
 const ActionTracks = () => {
     const { isLoggedIn } = useUserAPI();
-    const { isLoading, getActionTracks, actionTracksByDate, activeActionTracks } = useActionTrackContext();
+    const { isLoading: isLoadingActionTrack, getActionTracks, actionTracksByDate, activeActionTracks } = useActionTrackContext();
+    const { isLoading: isLoadingActions, getActions, actions } = useActionContext();
+
+    const isLoading = isLoadingActionTrack || isLoadingActions;
 
     useEffect(() => {
         if ((actionTracksByDate === undefined || activeActionTracks === undefined) && !isLoading && isLoggedIn) getActionTracks();
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [actionTracksByDate, activeActionTracks, getActionTracks]);
+
+    useEffect(() => {
+        if (actions === undefined && !isLoading && isLoggedIn) getActions();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [actions, getActions]);
     return (
         <BasePage isLoading={isLoading} pageName='ActionTracks'>
             <Box sx={{ pb: 4 }}>
+                <Grid container spacing={1} sx={{ pb: 2 }}>
+                    {actions?.map(action => (
+                        <Grid size={6} key={action.id}>
+                            <Card sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', pl: 1 }}>
+                                <Typography variant='body2'>{action.name}</Typography>
+                                <IconButton size='small'>
+                                    <PlayArrowIcon />
+                                </IconButton>
+                            </Card>
+                        </Grid>
+                    ))}
+                </Grid>
                 <Grid container spacing={0.5}>
                     {actionTracksByDate?.map(actionTracks => (
                         <>
                             <Typography key={`date-${actionTracks[0].date}`}>{actionTracks[0].date}</Typography>
                             {actionTracks
-                                .filter(actionTrack => actionTrack.ended_at !== null)
+                                // .filter(actionTrack => actionTrack.ended_at !== null)
                                 .map(actionTrack => (
                                     <ActionTrack key={actionTrack.id} actionTrack={actionTrack} />
                                 ))}
