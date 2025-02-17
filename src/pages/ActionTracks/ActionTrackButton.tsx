dde31fd09c5e4cbc03da1971c7f83c0f38a70bc3
lynxlevin <72173230+lynxlevin@users.noreply.github.com>
2025-02-17 15:24:47 +0900

    Remove unused variable

--- src/pages/ActionTracks/ActionTrackButton.tsx
+++ src/pages/ActionTracks/ActionTrackButton.tsx
@@ -26,7 +26,6 @@ const ActionTrackButton = ({ action }: ActionTrackButtonProps) => {
         if (duration === undefined) return '';
         const hours = Math.floor(duration / 3600);
         const minutes = Math.floor((duration % 3600) / 60);
-        const seconds = Math.floor((duration % 3600) % 60);
         return `(${hours}:${zeroPad(minutes)})`;
     };
 
