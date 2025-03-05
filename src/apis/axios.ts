a3e77b70eac7b17dc81bccb8d0018fc957f40d61
lynxlevin <72173230+lynxlevin@users.noreply.github.com>
2025-03-05 20:35:16 +0900

    feat: Add redirect to Login page to axios config on receiving 401

--- src/apis/axios.ts
+++ src/apis/axios.ts
@@ -1,14 +1,26 @@
-import axios from 'axios';
+import axios, { type AxiosError } from 'axios';
 
 const client = axios.create({
     withCredentials: true,
 });
 
-client.interceptors.request.use(async config => {
+client.interceptors.request.use(config => {
     if (config.method !== undefined && ['post', 'put'].includes(config.method)) {
         config.headers['content-type'] = 'application/json';
     }
     return config;
 });
+
+client.interceptors.response.use(
+    response => {
+        return response;
+    },
+    async (err: AxiosError) => {
+        if (err.response && err.response.status === 401) {
+            if (window.location.pathname !== '/login') window.location.pathname = '/login';
+        }
+        return await Promise.reject(err);
+    },
+);
 
 export default client;
