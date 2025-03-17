aa82d7ba40af7e78ec62bc7edc3c3b3149f4d99e
lynxlevin <72173230+lynxlevin@users.noreply.github.com>
2025-03-17 21:06:36 +0900

    feat: DiaryContext hook

---
+++ src/hooks/useDiaryContext.tsx
@@ -0,0 +1,61 @@
+import { useCallback, useContext, useState } from 'react';
+import { DiaryAPI } from '../apis/DiaryAPI';
+import { DiaryContext, SetDiaryContext } from '../contexts/diary-context';
+import { format } from 'date-fns';
+import type { DiaryKey } from '../types/diary';
+
+const useDiaryContext = () => {
+    const diaryContext = useContext(DiaryContext);
+    const setDiaryContext = useContext(SetDiaryContext);
+
+    const [isLoading, setIsLoading] = useState(false);
+
+    const diaries = diaryContext.diaryList;
+    const clearDiariesCache = () => {
+        setDiaryContext.setDiaryList(undefined);
+    };
+
+    const getDiaries = useCallback(() => {
+        setIsLoading(true);
+        DiaryAPI.list()
+            .then(res => {
+                setDiaryContext.setDiaryList(res.data);
+            })
+            .catch(e => {
+                console.error(e);
+            })
+            .finally(() => {
+                setIsLoading(false);
+            });
+    }, [setDiaryContext]);
+
+    const createDiary = (text: string | null, date: Date, score: number | null, tag_ids: string[]) => {
+        DiaryAPI.create({ text, date: format(date, 'yyyy-MM-dd'), score, tag_ids }).then(_ => {
+            getDiaries();
+        });
+    };
+
+    const updateDiary = (id: string, text: string | null, date: Date, score: number | null, tag_ids: string[], update_keys: DiaryKey[]) => {
+        DiaryAPI.update(id, { text, date: format(date, 'yyyy-MM-dd'), score, tag_ids, update_keys }).then(_ => {
+            getDiaries();
+        });
+    };
+
+    const deleteDiary = (id: string) => {
+        DiaryAPI.delete(id).then(_ => {
+            getDiaries();
+        });
+    };
+
+    return {
+        isLoading,
+        diaries,
+        clearDiariesCache,
+        getDiaries,
+        createDiary,
+        updateDiary,
+        deleteDiary,
+    };
+};
+
+export default useDiaryContext;
