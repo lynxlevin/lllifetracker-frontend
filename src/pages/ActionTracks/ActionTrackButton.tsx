42a4fd58f305e3f36a0ecb7b4d01d7c24f571b6f
lynxlevin <72173230+lynxlevin@users.noreply.github.com>
2025-03-24 15:38:00 +0900

    fix (ActionTrackButton): long action_name

--- src/pages/ActionTracks/ActionTrackButton.tsx
+++ src/pages/ActionTracks/ActionTrackButton.tsx
@@ -32,7 +32,7 @@ const ActionTrackButton = ({ action }: ActionTrackButtonProps) => {
     return (
         <Grid size={6}>
             <Card sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', pl: 1 }} onClick={startTracking}>
-                <Typography variant='body2' align='left'>
+                <Typography variant='body2' align='left' sx={{ whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis' }}>
                     <span style={{ color: action.color }}>⚫︎</span>
                     {action.name} {getDuration(totalForToday)}
                 </Typography>
