9cbc25fd3fecfdbba22ea67d6a52170f78542f63
lynxlevin <72173230+lynxlevin@users.noreply.github.com>
2025-03-30 12:24:13 +0900

    choir (Home): Change name of DesiredState

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -197,7 +197,7 @@ const Home = () => {
                     <Stack direction='row' mt={0.5}>
                         <DesiredStateIcon />
                         <Typography variant='h6' textAlign='left'>
-                            目指す姿
+                            目指す姿 / 目標
                         </Typography>
                     </Stack>
                     <Stack direction='row'>
