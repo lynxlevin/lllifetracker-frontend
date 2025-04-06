6c8eaedce4e7042aa3c896aaaa9d2948e8bf01b5
lynxlevin <72173230+lynxlevin@users.noreply.github.com>
2025-04-07 08:20:01 +0900

    feat: Open Focus dialog on top of the screen

--- src/pages/ActionTracks/Dialogs/ActionFocusDialog.tsx
+++ src/pages/ActionTracks/Dialogs/ActionFocusDialog.tsx
@@ -1,4 +1,4 @@
-import { Dialog, DialogContent, Typography } from '@mui/material';
+import { Dialog, DialogContent, type SxProps, Typography } from '@mui/material';
 import type { Action } from '../../../types/action';
 
 interface ActionFocusDialogProps {
@@ -7,8 +7,11 @@ interface ActionFocusDialogProps {
 }
 
 const ActionFocusDialog = ({ onClose, action }: ActionFocusDialogProps) => {
+    const sx: SxProps = {
+        '& .MuiDialog-container': { alignItems: 'flex-start' },
+    };
     return (
-        <Dialog open={true} onClose={onClose} fullWidth>
+        <Dialog open={true} onClose={onClose} fullWidth sx={sx}>
             <DialogContent>
                 <Typography variant='body1' sx={{ textShadow: 'lightgrey 0.4px 0.4px 0.5px', mb: 1, lineHeight: '1em' }}>
                     {action.name}
