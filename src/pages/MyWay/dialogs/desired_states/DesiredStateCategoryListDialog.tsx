affa3a5bcce398279cbf26705316183cd9ebc4d2
lynxlevin <72173230+lynxlevin@users.noreply.github.com>
2025-06-16 10:04:25 +0900

    feat: category sort

--- src/pages/MyWay/dialogs/desired_states/DesiredStateCategoryListDialog.tsx
+++ src/pages/MyWay/dialogs/desired_states/DesiredStateCategoryListDialog.tsx
@@ -1,8 +1,10 @@
 import {
     AppBar,
     Box,
+    Button,
     Container,
     Dialog,
+    DialogActions,
     DialogContent,
     IconButton,
     ListItemIcon,
@@ -14,8 +16,11 @@ import {
     Toolbar,
     Typography,
 } from '@mui/material';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import type { DesiredStateCategory } from '../../../../types/my_way';
+import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
+import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
+import SortIcon from '@mui/icons-material/Sort';
 import MenuIcon from '@mui/icons-material/Menu';
 import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
@@ -25,14 +30,17 @@ import useDesiredStateCategoryContext from '../../../../hooks/useDesiredStateCat
 import ConfirmationDialog from '../../../../components/ConfirmationDialog';
 import useDesiredStateContext from '../../../../hooks/useDesiredStateContext';
 import DesiredStateCategoryDialog from './DesiredStateCategoryDialog';
+import { moveItemDown, moveItemUp } from '../../../../hooks/useArraySort';
 
 interface DesiredStateCategoryListDialogProps {
     onClose: () => void;
 }
 
 const DesiredStateCategoryListDialog = ({ onClose }: DesiredStateCategoryListDialogProps) => {
-    const { desiredStateCategories } = useDesiredStateCategoryContext();
+    const { desiredStateCategories: categoriesMaster, bulkUpdateDesiredStateCategoryOrdering, getDesiredStateCategories } = useDesiredStateCategoryContext();
+    const [categories, setCategories] = useState<DesiredStateCategory[]>([]);
     const [openedDialog, setOpenedDialog] = useState<'Create'>();
+    const [isSortMode, setIsSortMode] = useState(false);
 
     const getDialog = () => {
         switch (openedDialog) {
@@ -46,6 +54,26 @@ const DesiredStateCategoryListDialog = ({ onClose }: DesiredStateCategoryListDia
                 );
         }
     };
+
+    const saveSorting = () => {
+        if (categories.length === 0) return;
+        bulkUpdateDesiredStateCategoryOrdering(categories.map(category => category.id)).then(_ => {
+            getDesiredStateCategories();
+            setIsSortMode(false);
+        });
+    };
+
+    const cancelSorting = () => {
+        setCategories(categoriesMaster!);
+        setIsSortMode(false);
+    };
+
+    useEffect(() => {
+        if (categories.length === 0 && categoriesMaster !== undefined && categoriesMaster.length > 0) {
+            setCategories(categoriesMaster);
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [categoriesMaster]);
 
     return (
         <Dialog open={true} onClose={onClose} fullScreen>
@@ -64,27 +92,71 @@ const DesiredStateCategoryListDialog = ({ onClose }: DesiredStateCategoryListDia
                         <Stack direction='row' justifyContent='space-between'>
                             <Box />
                             <Stack direction='row'>
-                                <IconButton onClick={() => setOpenedDialog('Create')} aria-label='add' color='primary'>
+                                <IconButton onClick={() => setIsSortMode(true)} aria-label='add' color='primary'>
+                                    <SortIcon />
+                                </IconButton>
+                                <IconButton onClick={() => setOpenedDialog('Create')} disabled={isSortMode} aria-label='add' color='primary'>
                                     <AddCircleOutlineOutlinedIcon />
                                 </IconButton>
                             </Stack>
                         </Stack>
-                        {desiredStateCategories?.map(category => (
-                            <DesiredStateCategoryItem key={category.id} category={category} />
+                        {categories?.map((category, idx) => (
+                            <DesiredStateCategoryItem
+                                key={category.id}
+                                category={category}
+                                isSortMode={isSortMode}
+                                idx={idx}
+                                categoriesLength={categories.length}
+                                setCategories={setCategories}
+                            />
                         ))}
                     </Box>
                 </Container>
                 {openedDialog && getDialog()}
             </DialogContent>
+            {isSortMode && (
+                <DialogActions sx={{ justifyContent: 'center', pb: 2, bgcolor: 'background.default', borderTop: '1px solid #ccc' }}>
+                    <>
+                        <Button variant='outlined' onClick={cancelSorting} sx={{ color: 'primary.dark' }}>
+                            キャンセル
+                        </Button>
+                        <Button variant='contained' onClick={saveSorting}>
+                            保存する
+                        </Button>
+                    </>
+                </DialogActions>
+            )}
         </Dialog>
     );
 };
 
-const DesiredStateCategoryItem = ({ category }: { category: DesiredStateCategory }) => {
+const DesiredStateCategoryItem = ({
+    category,
+    isSortMode,
+    idx,
+    categoriesLength,
+    setCategories,
+}: {
+    category: DesiredStateCategory;
+    isSortMode: boolean;
+    idx: number;
+    categoriesLength: number;
+    setCategories: (value: React.SetStateAction<DesiredStateCategory[]>) => void;
+}) => {
     const { deleteDesiredStateCategory } = useDesiredStateCategoryContext();
     const { getDesiredStates } = useDesiredStateContext();
     const [openedDialog, setOpenedDialog] = useState<'Edit' | 'Delete'>();
     const [menuAnchor, setMenuAnchor] = useState<null | HTMLElement>(null);
+
+    const handleUp = (idx: number) => {
+        if (idx === 0) return;
+        setCategories(prev => moveItemUp(prev, idx));
+    };
+
+    const handleDown = (idx: number) => {
+        if (idx === categoriesLength - 1) return;
+        setCategories(prev => moveItemDown(prev, idx));
+    };
 
     const getDialog = () => {
         switch (openedDialog) {
@@ -116,46 +188,72 @@ const DesiredStateCategoryItem = ({ category }: { category: DesiredStateCategory
         }
     };
     return (
-        <Paper key={category.id} sx={{ py: 1, px: 2, my: 1 }}>
-            <Stack direction='row' justifyContent='space-between'>
-                <Typography variant='body1' sx={{ textShadow: 'lightgrey 0.4px 0.4px 0.5px', mt: 1, lineHeight: '1em' }}>
-                    {category.name}
-                </Typography>
-                <IconButton
-                    size='small'
-                    onClick={event => {
-                        setMenuAnchor(event.currentTarget);
-                    }}
-                >
-                    <MenuIcon />
-                </IconButton>
-                <Menu anchorEl={menuAnchor} open={Boolean(menuAnchor)} onClose={() => setMenuAnchor(null)}>
-                    <MenuItem
+        <Stack direction='row'>
+            <Paper key={category.id} sx={{ py: 1, px: 2, my: 0.5, width: '100%' }}>
+                <Stack direction='row' justifyContent='space-between' alignItems='center'>
+                    <Typography variant='body1' sx={{ textShadow: 'lightgrey 0.4px 0.4px 0.5px', lineHeight: '1.5em' }}>
+                        {category.name}
+                    </Typography>
+                    {!isSortMode && (
+                        <IconButton
+                            size='small'
+                            onClick={event => {
+                                setMenuAnchor(event.currentTarget);
+                            }}
+                        >
+                            <MenuIcon />
+                        </IconButton>
+                    )}
+                    <Menu anchorEl={menuAnchor} open={Boolean(menuAnchor)} onClose={() => setMenuAnchor(null)}>
+                        <MenuItem
+                            onClick={() => {
+                                setMenuAnchor(null);
+                                setOpenedDialog('Edit');
+                            }}
+                        >
+                            <ListItemIcon>
+                                <EditIcon />
+                            </ListItemIcon>
+                            <ListItemText>編集</ListItemText>
+                        </MenuItem>
+                        <MenuItem
+                            onClick={() => {
+                                setMenuAnchor(null);
+                                setOpenedDialog('Delete');
+                            }}
+                        >
+                            <ListItemIcon>
+                                <DeleteIcon />
+                            </ListItemIcon>
+                            <ListItemText>削除</ListItemText>
+                        </MenuItem>
+                    </Menu>
+                </Stack>
+            </Paper>
+            {isSortMode && (
+                <>
+                    <IconButton
+                        size='small'
                         onClick={() => {
-                            setMenuAnchor(null);
-                            setOpenedDialog('Edit');
+                            handleUp(idx);
                         }}
+                        disabled={idx === 0}
                     >
-                        <ListItemIcon>
-                            <EditIcon />
-                        </ListItemIcon>
-                        <ListItemText>編集</ListItemText>
-                    </MenuItem>
-                    <MenuItem
+                        <ArrowUpwardIcon />
+                    </IconButton>
+                    <IconButton
+                        size='small'
                         onClick={() => {
-                            setMenuAnchor(null);
-                            setOpenedDialog('Delete');
+                            handleDown(idx);
                         }}
+                        disabled={idx === categoriesLength - 1}
                     >
-                        <ListItemIcon>
-                            <DeleteIcon />
-                        </ListItemIcon>
-                        <ListItemText>削除</ListItemText>
-                    </MenuItem>
-                </Menu>
-            </Stack>
+                        <ArrowDownwardIcon />
+                    </IconButton>
+                </>
+            )}
             {openedDialog && getDialog()}
-        </Paper>
+        </Stack>
     );
 };
 
