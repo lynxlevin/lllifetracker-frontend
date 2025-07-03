eb65c3bcd8e2703fe9467b89e162576c9432de22
lynxlevin <72173230+lynxlevin@users.noreply.github.com>
2025-07-03 16:00:51 +0900

    feat (WeeklyAggregations): Divide graph and table into tabs

--- src/pages/ActionTrackAggregations/WeeklyAggregations.tsx
+++ src/pages/ActionTrackAggregations/WeeklyAggregations.tsx
@@ -1,4 +1,4 @@
-import { Box, Stack, Typography, IconButton, Select, MenuItem, type SelectChangeEvent, CircularProgress, styled, Button } from '@mui/material';
+import { Box, Stack, Typography, IconButton, Select, MenuItem, type SelectChangeEvent, CircularProgress, styled, Button, Tabs, Tab } from '@mui/material';
 import { useEffect, useMemo, useState } from 'react';
 import BasePage from '../../components/BasePage';
 import useActionContext from '../../hooks/useActionContext';
@@ -13,12 +13,14 @@ import type { DurationsByAction } from '../../types/action_track';
 import { type BarLabelProps, BarPlot, ChartContainer, ChartsXAxis, ChartsYAxis, useAnimate } from '@mui/x-charts';
 import { interpolateObject } from '@mui/x-charts-vendor/d3-interpolate';
 
+type TabType = 'graph' | 'table';
+
 const WeeklyAggregations = () => {
     const { dailyAggregation, getDailyAggregations, findMonthFromDailyAggregation } = useActionTrackContext();
     const { isLoading: isLoadingActions, actions, getActions } = useActionContext();
 
     const [selectedDate, setSelectedDate] = useState(new Date());
-    const [selectedAction, setSelectedAction] = useState<Action>();
+    const [selectedTab, setSelectedTab] = useState<TabType>('graph');
 
     const daysForSelectedWeek = useMemo(() => {
         const start = startOfWeek(selectedDate);
@@ -68,16 +70,6 @@ const WeeklyAggregations = () => {
         return res.length === 0 ? undefined : res;
     }, [dailyAggregation, selectedWeekAggregationByDay]);
 
-    const zeroPad = (num: number) => {
-        return num.toString().padStart(2, '0');
-    };
-    const getDuration = (minutesTotal: number | null) => {
-        if (minutesTotal === null || minutesTotal === 0) return '';
-        const hours = Math.floor(minutesTotal / 60);
-        const minutes = Math.floor(minutesTotal % 60);
-        return `${hours}:${zeroPad(minutes)}`;
-    };
-
     useEffect(() => {
         const start = startOfWeek(selectedDate);
         const end = endOfWeek(selectedDate);
@@ -92,13 +84,9 @@ const WeeklyAggregations = () => {
         if (actions === undefined && !isLoadingActions) getActions();
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [actions, getActions]);
-    useEffect(() => {
-        if (actions === undefined) return;
-        if (selectedAction === undefined) setSelectedAction(actions[0]);
-    }, [actions, selectedAction]);
     return (
         <BasePage pageName='Aggregation'>
-            <Box sx={{ pb: 12, pt: 4 }}>
+            <Box sx={{ pt: 4 }}>
                 <Stack direction='row' justifyContent='center' alignItems='center'>
                     <IconButton
                         onClick={() => {
@@ -128,60 +116,123 @@ const WeeklyAggregations = () => {
                         <KeyboardArrowRightIcon />
                     </IconButton>
                 </Stack>
-                <Box sx={{ mt: 2 }}>
-                    {selectedAction === undefined ? (
-                        <CircularProgress style={{ marginRight: 'auto', marginLeft: 'auto' }} />
-                    ) : (
-                        <>
-                            <Select
-                                value={selectedAction?.id}
-                                onChange={(event: SelectChangeEvent<string>) => setSelectedAction(actions?.find(action => action.id === event.target.value))}
-                            >
-                                {actions?.map(action => (
-                                    <MenuItem key={action.id} value={action.id}>
-                                        {action.name}
-                                    </MenuItem>
-                                ))}
-                            </Select>
-                            <ChartContainer
-                                height={200}
-                                width={400}
-                                style={{ translate: '-60px 0' }}
-                                series={[
-                                    {
-                                        data: selectedWeekAggregationByDay.map(dateAgg =>
-                                            selectedAction.track_type === 'TimeSpan'
-                                                ? (dateAgg.find(agg => agg.action_id === selectedAction.id)?.duration ?? 0) / 60
-                                                : (dateAgg.find(agg => agg.action_id === selectedAction.id)?.count ?? 0),
-                                        ),
-                                        label: selectedAction.track_type === 'TimeSpan' ? '時間(分)' : '回数',
-                                        type: 'bar',
-                                    },
-                                ]}
-                                colors={[selectedAction.color ?? orange[500]]}
-                                xAxis={[{ scaleType: 'band', data: daysForSelectedWeek.map(date => `${date.getMonth() + 1}/${date.getDate()}`) }]}
-                            >
-                                <BarPlot
-                                    barLabel={(item, _) => {
-                                        return selectedAction.track_type === 'TimeSpan'
-                                            ? getDuration(item.value)
-                                            : item.value === 0
-                                              ? ''
-                                              : item.value?.toString();
-                                    }}
-                                    slots={{ barLabel: BarLabel }}
-                                />
-                                <ChartsXAxis disableTicks />
-                                <ChartsYAxis tickLabelStyle={{ display: 'none' }} disableLine disableTicks />
-                            </ChartContainer>
-                        </>
-                    )}
-                </Box>
-                <Box sx={{ mt: 2 }}>
-                    <BasicAggregation aggregations={selectedWeekAggregationTotal} selectedDatesCount={7} />
-                </Box>
+                <Tabs
+                    value={selectedTab}
+                    onChange={(_, newValue: TabType) => {
+                        setSelectedTab(newValue);
+                    }}
+                    centered
+                >
+                    <Tab label='グラフ' value={'graph' as TabType} />
+                    <Tab label='統計' value={'table' as TabType} />
+                </Tabs>
+                {selectedTab === 'graph' && <WeeklyAggregationsGraph selectedDate={selectedDate} />}
+                {selectedTab === 'table' && (
+                    <Box sx={{ mt: 2 }}>
+                        <BasicAggregation aggregations={selectedWeekAggregationTotal} selectedDatesCount={7} />
+                    </Box>
+                )}
             </Box>
         </BasePage>
+    );
+};
+
+const WeeklyAggregationsGraph = ({ selectedDate }: { selectedDate: Date }) => {
+    const { dailyAggregation, findMonthFromDailyAggregation } = useActionTrackContext();
+    const { actions } = useActionContext();
+
+    const [selectedAction, setSelectedAction] = useState<Action>();
+
+    const daysForSelectedWeek = useMemo(() => {
+        const start = startOfWeek(selectedDate);
+        return [
+            start,
+            add(start, { days: 1 }),
+            add(start, { days: 2 }),
+            add(start, { days: 3 }),
+            add(start, { days: 4 }),
+            add(start, { days: 5 }),
+            add(start, { days: 6 }),
+        ];
+    }, [selectedDate]);
+
+    const selectedWeekAggregationByDay = useMemo(() => {
+        if (dailyAggregation === undefined) return [];
+
+        const res: DurationsByAction[][] = [];
+        for (const day of daysForSelectedWeek) {
+            const selectedMonthAgg = findMonthFromDailyAggregation(day);
+            if (selectedMonthAgg === undefined) continue;
+            const selectedDateAgg = selectedMonthAgg.find(date => date.date === day.getDate());
+            if (selectedDateAgg === undefined) {
+                res.push([]);
+                continue;
+            }
+            res.push(selectedDateAgg.aggregation);
+        }
+        return res;
+    }, [dailyAggregation, daysForSelectedWeek, findMonthFromDailyAggregation]);
+
+    const zeroPad = (num: number) => {
+        return num.toString().padStart(2, '0');
+    };
+    const getDuration = (minutesTotal: number | null) => {
+        if (minutesTotal === null || minutesTotal === 0) return '';
+        const hours = Math.floor(minutesTotal / 60);
+        const minutes = Math.floor(minutesTotal % 60);
+        return `${hours}:${zeroPad(minutes)}`;
+    };
+
+    useEffect(() => {
+        if (actions === undefined) return;
+        if (selectedAction === undefined) setSelectedAction(actions[0]);
+    }, [actions, selectedAction]);
+    return (
+        <Box sx={{ mt: 2 }}>
+            {selectedAction === undefined ? (
+                <CircularProgress style={{ marginRight: 'auto', marginLeft: 'auto' }} />
+            ) : (
+                <>
+                    <Select
+                        value={selectedAction?.id}
+                        onChange={(event: SelectChangeEvent<string>) => setSelectedAction(actions?.find(action => action.id === event.target.value))}
+                    >
+                        {actions?.map(action => (
+                            <MenuItem key={action.id} value={action.id}>
+                                {action.name}
+                            </MenuItem>
+                        ))}
+                    </Select>
+                    <ChartContainer
+                        height={200}
+                        width={400}
+                        style={{ translate: '-60px 0' }}
+                        series={[
+                            {
+                                data: selectedWeekAggregationByDay.map(dateAgg =>
+                                    selectedAction.track_type === 'TimeSpan'
+                                        ? (dateAgg.find(agg => agg.action_id === selectedAction.id)?.duration ?? 0) / 60
+                                        : (dateAgg.find(agg => agg.action_id === selectedAction.id)?.count ?? 0),
+                                ),
+                                label: selectedAction.track_type === 'TimeSpan' ? '時間(分)' : '回数',
+                                type: 'bar',
+                            },
+                        ]}
+                        colors={[selectedAction.color ?? orange[500]]}
+                        xAxis={[{ scaleType: 'band', data: daysForSelectedWeek.map(date => `${date.getMonth() + 1}/${date.getDate()}`) }]}
+                    >
+                        <BarPlot
+                            barLabel={(item, _) => {
+                                return selectedAction.track_type === 'TimeSpan' ? getDuration(item.value) : item.value === 0 ? '' : item.value?.toString();
+                            }}
+                            slots={{ barLabel: BarLabel }}
+                        />
+                        <ChartsXAxis disableTicks />
+                        <ChartsYAxis tickLabelStyle={{ display: 'none' }} disableLine disableTicks />
+                    </ChartContainer>
+                </>
+            )}
+        </Box>
     );
 };
 
