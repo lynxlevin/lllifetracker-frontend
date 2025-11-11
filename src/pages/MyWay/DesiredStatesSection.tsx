3193f99b660aa7d6dd687ef89d704d7112d1c451
lynxlevin <72173230+lynxlevin@users.noreply.github.com>
2025-11-11 20:33:14 +0900

    feat (DesiredStates): category tab toggle

--- src/pages/MyWay/DesiredStatesSection.tsx
+++ src/pages/MyWay/DesiredStatesSection.tsx
@@ -51,9 +51,11 @@ const DesiredStatesSection = () => {
 
     const mapDesiredStates = () => {
         if (desiredStates === undefined || isLoadingDesiredState) return <CircularProgress style={{ marginRight: 'auto', marginLeft: 'auto' }} />;
-        const filtered = desiredStates.filter(
-            desiredState => (selectedCategoryId === FOCUS_ITEMS && desiredState.is_focused) || desiredState.category_id === selectedCategoryId,
-        );
+        const filtered = desiredStatesDisplayMode.categoryTab
+            ? desiredStates.filter(
+                  desiredState => (selectedCategoryId === FOCUS_ITEMS && desiredState.is_focused) || desiredState.category_id === selectedCategoryId,
+              )
+            : desiredStates;
         if (selectedCategoryId !== FOCUS_ITEMS && filtered.length === 0)
             return (
                 <Button variant="outlined" fullWidth onClick={() => setOpenedDialog('Create')}>
@@ -61,34 +63,41 @@ const DesiredStatesSection = () => {
                 </Button>
             );
 
+        let lastCategoryId: string | null;
         switch (desiredStatesDisplayMode.item) {
             case 'Full':
                 return filtered.map(desiredState => {
+                    const shouldShowCategory = !desiredStatesDisplayMode.categoryTab && lastCategoryId !== desiredState.category_id;
+                    lastCategoryId = desiredState.category_id;
                     return (
                         <DesiredStateItem
                             key={desiredState.id}
                             desiredState={desiredState}
-                            showCategory={selectedCategoryId === FOCUS_ITEMS}
+                            showCategory={selectedCategoryId === FOCUS_ITEMS && desiredStatesDisplayMode.categoryTab}
                             displayMode={desiredStatesDisplayMode}
                             onClick={() => {
                                 setOpenedDialog('Details');
                                 setSelectedDesiredStateId(desiredState.id);
                             }}
+                            shouldShowCategory={shouldShowCategory}
                         />
                     );
                 });
             case 'TitleOnly':
                 return filtered.map(desiredState => {
+                    const shouldShowCategory = !desiredStatesDisplayMode.categoryTab && lastCategoryId !== desiredState.category_id;
+                    lastCategoryId = desiredState.category_id;
                     return (
                         <DesiredStateItem
                             key={desiredState.id}
                             desiredState={desiredState}
-                            showCategory={selectedCategoryId === FOCUS_ITEMS}
+                            showCategory={selectedCategoryId === FOCUS_ITEMS && desiredStatesDisplayMode.categoryTab}
                             displayMode={desiredStatesDisplayMode}
                             onClick={() => {
                                 setOpenedDialog('Details');
                                 setSelectedDesiredStateId(desiredState.id);
                             }}
+                            shouldShowCategory={shouldShowCategory}
                         />
                     );
                 });
@@ -247,13 +256,15 @@ const DesiredStatesSection = () => {
                 <CircularProgress style={{ marginRight: 'auto', marginLeft: 'auto' }} />
             ) : (
                 <>
-                    <Tabs value={selectedCategoryId} onChange={onSelectCategory} variant="scrollable" scrollButtons allowScrollButtonsMobile>
-                        <Tab label="フォーカス" value={FOCUS_ITEMS} />
-                        {desiredStateCategories!.map(category => {
-                            return <Tab key={category.id} label={category.name} value={category.id} />;
-                        })}
-                        {showNoCategory && <Tab label="なし" value={null} />}
-                    </Tabs>
+                    {desiredStatesDisplayMode.categoryTab && (
+                        <Tabs value={selectedCategoryId} onChange={onSelectCategory} variant="scrollable" scrollButtons allowScrollButtonsMobile>
+                            <Tab label="フォーカス" value={FOCUS_ITEMS} />
+                            {desiredStateCategories!.map(category => {
+                                return <Tab key={category.id} label={category.name} value={category.id} />;
+                            })}
+                            {showNoCategory && <Tab label="なし" value={null} />}
+                        </Tabs>
+                    )}
                     <Stack spacing={1} sx={{ textAlign: 'left', mt: 1, minHeight: '50px' }}>
                         {mapDesiredStates()}
                     </Stack>
@@ -269,42 +280,51 @@ const DesiredStateItem = ({
     showCategory,
     onClick,
     displayMode,
+    shouldShowCategory,
 }: {
     desiredState: DesiredState;
     showCategory: boolean;
     onClick: () => void;
     displayMode: DesiredStatesDisplayMode;
+    shouldShowCategory: boolean;
 }) => {
     const { desiredStateCategories } = useDesiredStateCategoryContext();
 
     const category = desiredStateCategories!.find(category => category.id === desiredState.category_id);
 
     return (
-        <Paper sx={{ py: 1, px: 2, position: 'relative' }} onClick={onClick}>
-            {desiredState.is_focused && <StarsIcon sx={{ position: 'absolute', top: '-2px', left: 0, fontSize: '1.2rem', color: yellow[700] }} />}
-            <Stack direction="row" justifyContent="space-between">
-                <div>
-                    {showCategory && category && (
-                        <Typography variant="body2" fontWeight={100}>
-                            {category.name}
-                        </Typography>
-                    )}
-                    <Typography variant="body1" sx={{ textShadow: 'lightgrey 0.4px 0.4px 0.5px' }}>
-                        {desiredState.name}
-                    </Typography>
-                </div>
-                {displayMode.item === 'TitleOnly' && (
-                    <Stack direction="row" alignItems="center">
-                        <InfoIcon sx={{ color: grey[500], fontSize: '1.2em' }} />
-                    </Stack>
-                )}
-            </Stack>
-            {displayMode.item === 'Full' && (
-                <Typography variant="body2" sx={{ whiteSpace: 'pre-wrap', fontWeight: 100 }}>
-                    {desiredState.description}
+        <>
+            {shouldShowCategory && (
+                <Typography fontSize="1rem" mt={1}>
+                    {category?.name ?? 'カテゴリーなし'}
                 </Typography>
             )}
-        </Paper>
+            <Paper sx={{ py: 1, px: 2, position: 'relative' }} onClick={onClick}>
+                {desiredState.is_focused && <StarsIcon sx={{ position: 'absolute', top: '-2px', left: 0, fontSize: '1.2rem', color: yellow[700] }} />}
+                <Stack direction="row" justifyContent="space-between">
+                    <div>
+                        {showCategory && category && (
+                            <Typography variant="body2" fontWeight={100}>
+                                {category.name}
+                            </Typography>
+                        )}
+                        <Typography variant="body1" sx={{ textShadow: 'lightgrey 0.4px 0.4px 0.5px' }}>
+                            {desiredState.name}
+                        </Typography>
+                    </div>
+                    {displayMode.item === 'TitleOnly' && (
+                        <Stack direction="row" alignItems="center">
+                            <InfoIcon sx={{ color: grey[500], fontSize: '1.2em' }} />
+                        </Stack>
+                    )}
+                </Stack>
+                {displayMode.item === 'Full' && (
+                    <Typography variant="body2" sx={{ whiteSpace: 'pre-wrap', fontWeight: 100 }}>
+                        {desiredState.description}
+                    </Typography>
+                )}
+            </Paper>
+        </>
     );
 };
 
