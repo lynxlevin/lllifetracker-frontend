307a8bf5ba4567a396adb71bf7be91ab767881dd
lynxlevin <72173230+lynxlevin@users.noreply.github.com>
2026-05-20 21:53:30 +0900

    chore: remove aggregationForTheDay

--- src/contexts/action-track-context.tsx
+++ src/contexts/action-track-context.tsx
@@ -1,43 +1,38 @@
 import { createContext, useState, type ReactNode } from 'react';
-import type { ActionTrack, ActionTrackAggregation, ActionTrackDailyAggregation } from '../types/action_track';
+import type { ActionTrack, ActionTrackDailyAggregation } from '../types/action_track';
 
 interface ActionTrackContextType {
     activeActionTrackList: ActionTrack[] | undefined;
     actionTracksForTheDay: ActionTrack[] | undefined;
-    aggregationForTheDay: ActionTrackAggregation | undefined;
     dailyAggregation: ActionTrackDailyAggregation | undefined;
 }
 
 interface SetActionTrackContextType {
     setActiveActionTrackList: React.Dispatch<React.SetStateAction<ActionTrack[] | undefined>>;
     setActionTracksForTheDay: React.Dispatch<React.SetStateAction<ActionTrack[] | undefined>>;
-    setAggregationForTheDay: React.Dispatch<React.SetStateAction<ActionTrackAggregation | undefined>>;
     setDailyAggregation: React.Dispatch<React.SetStateAction<ActionTrackDailyAggregation | undefined>>;
 }
 
 export const ActionTrackContext = createContext<ActionTrackContextType>({
     activeActionTrackList: undefined,
     actionTracksForTheDay: undefined,
-    aggregationForTheDay: undefined,
     dailyAggregation: undefined,
 });
 
 export const SetActionTrackContext = createContext<SetActionTrackContextType>({
     setActiveActionTrackList: () => {},
     setActionTracksForTheDay: () => {},
-    setAggregationForTheDay: () => {},
     setDailyAggregation: () => {},
 });
 
 export const ActionTrackProvider = ({ children }: { children: ReactNode }) => {
     const [activeActionTrackList, setActiveActionTrackList] = useState<ActionTrack[]>();
     const [actionTracksForTheDay, setActionTracksForTheDay] = useState<ActionTrack[]>();
-    const [aggregationForTheDay, setAggregationForTheDay] = useState<ActionTrackAggregation>();
     const [dailyAggregation, setDailyAggregation] = useState<ActionTrackDailyAggregation>();
 
     return (
-        <ActionTrackContext.Provider value={{ activeActionTrackList, actionTracksForTheDay, aggregationForTheDay, dailyAggregation }}>
-            <SetActionTrackContext.Provider value={{ setActiveActionTrackList, setActionTracksForTheDay, setAggregationForTheDay, setDailyAggregation }}>
+        <ActionTrackContext.Provider value={{ activeActionTrackList, actionTracksForTheDay, dailyAggregation }}>
+            <SetActionTrackContext.Provider value={{ setActiveActionTrackList, setActionTracksForTheDay, setDailyAggregation }}>
                 {children}
             </SetActionTrackContext.Provider>
         </ActionTrackContext.Provider>
