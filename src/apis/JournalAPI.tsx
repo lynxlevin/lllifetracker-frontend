641afe307086fdc4b8531d2e17b23abf3f2ec2e7
lynxlevin <72173230+lynxlevin@users.noreply.github.com>
2026-06-09 14:30:50 +0900

    feat (JournalAPI): search endpoint

--- src/apis/JournalAPI.tsx
+++ src/apis/JournalAPI.tsx
@@ -5,6 +5,14 @@ import type { AxiosResponse } from 'axios';
 interface JournalListQuery {
     tag_id_or?: string[];
 }
+export interface JournalSearchParams {
+    text?: string;
+    tag_ids: string[];
+}
+export const JOURNAL_SEARCH_PARAMS_DEFAULT = {
+    text: undefined,
+    tag_ids: [],
+};
 
 export const JournalAPI = {
     BASE_URL: '/api/journals',
@@ -15,5 +23,8 @@ export const JournalAPI = {
         if (query.tag_id_or) queries.push(`tag_id_or=${query.tag_id_or.join(',')}`);
         if (queries.length > 0) url += `?${queries.join('&')}`;
         return await client.get(url);
+    },
+    search: async (params: JournalSearchParams): Promise<AxiosResponse<Journal[]>> => {
+        return await client.post(`${JournalAPI.BASE_URL}/search`, params);
     },
 };
